466bc24bf87691e1c777944b0c1297a415a3a7e9
dwkula <dawid.kula98@gmail.com>
2021-09-04 20:14:18 +0200

    init commit

---
+++ exercises.js
@@ -0,0 +1,10 @@
+// 8 kyu not ES6
+function check(a, x) {
+    // https://www.codewars.com/kata/57cc975ed542d3148f00015b
+    return a.includes(x)
+}
+  
+function cockroachSpeed(s) {
+    // https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+    return Math.floor((s * 100000 / 3600)) // fast af :o
+  }
